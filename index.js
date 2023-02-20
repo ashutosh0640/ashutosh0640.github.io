1202d3af87c0475da75e43f88ae6f96dc908dc51
ashutosh0640 <ashulook1013@gmail.com>
2023-02-20 18:50:20 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-function nav_menu(){
-    var element = document.getElementById(menuDiv);
-    element.classList.toggle("menuDivStyle");
+
+function menuFunction(){
+    console.log("hello");
+    let menuHome = document.getElementById("menuHome");
+    menuHome.style.right = "5px";
+    menuHome.style.position = "fixed";
+    menuHome.style.display = "inline-block";
+    menuHome.style.transitionDuration = "1s";
+    
+    
+    let menuAbout = document.getElementById("menuAbout");
+    menuAbout.style.right = "5px";
+    menuAbout.style.position = "fixed";
+    menuAbout.style.display = "inline-block";
+    menuAbout.style.transitionDuration = "2s";
+    
+    let menuSkills = document.getElementById("menuSkills");
+    menuSkills.style.right = "5px";
+    menuSkills.style.position = "fixed";
+    menuSkills.style.display = "inline-block";
+    menuSkills.style.transitionDuration = "3s";
+    
+
+    let menuContact = document.getElementById("menuContact");
+    menuContact.style.right = "5px";
+    menuContact.style.position = "fixed";
+    menuContact.style.display = "inline-block";
+    menuContact.style.transitionDuration = "4s";
+
+    let menuProject = document.getElementById("menuProject");
+    menuProject.style.right = "5px";
+    menuProject.style.position = "fixed";
+    menuProject.style.display = "inline-block";
+    menuProject.style.transitionDuration = "5s";
+
+
+    let menuStat = document.getElementById("menuStat");
+    menuStat.style.right = "5px";
+    menuStat.style.position = "fixed";
+    menuStat.style.display = "inline-block";
+    menuStat.style.transitionDuration = "6s";
+
 }
