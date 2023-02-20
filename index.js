43e5b95f5f148cbd32744953a2c0ce70f1934bab
ashutosh0640 <ashulook1013@gmail.com>
2023-02-20 21:19:10 +0530

    udated

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-
+let menuBtn = document.getElementsByClassName(menuBtn);
 function menuFunction(){
     console.log("hello");
     let menuHome = document.getElementById("menuHome");
     menuHome.style.right = "5px";
     menuHome.style.position = "fixed";
     menuHome.style.display = "inline-block";
-    menuHome.style.transitionDuration = "1s";
+    menuHome.style.transition = "right 1s";
     
     
     let menuAbout = document.getElementById("menuAbout");
     menuAbout.style.right = "5px";
     menuAbout.style.position = "fixed";
     menuAbout.style.display = "inline-block";
-    menuAbout.style.transitionDuration = "2s";
+    menuAbout.style.transition = "right 2s";
     
     let menuSkills = document.getElementById("menuSkills");
     menuSkills.style.right = "5px";
     menuSkills.style.position = "fixed";
     menuSkills.style.display = "inline-block";
-    menuSkills.style.transitionDuration = "3s";
+    menuSkills.style.transition = "right 3s";
     
 
     let menuContact = document.getElementById("menuContact");
     menuContact.style.right = "5px";
     menuContact.style.position = "fixed";
     menuContact.style.display = "inline-block";
-    menuContact.style.transitionDuration = "4s";
+    menuContact.style.transition = "right 4s";
 
     let menuProject = document.getElementById("menuProject");
     menuProject.style.right = "5px";
     menuProject.style.position = "fixed";
     menuProject.style.display = "inline-block";
-    menuProject.style.transitionDuration = "5s";
+    menuProject.style.transition = "right5s";
 
 
     let menuStat = document.getElementById("menuStat");
     menuStat.style.right = "5px";
     menuStat.style.position = "fixed";
     menuStat.style.display = "inline-block";
-    menuStat.style.transitionDuration = "6s";
+   menuStat.style.transition = "right 6s";
 
 }
